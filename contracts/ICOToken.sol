38543f26227f89e7501bcc0fdf5e70a1196f518b
Zihui Lee <kyleezihui@gmail.com>
2023-07-18 03:00:33 +0000

    Add 3 functions

--- contracts/ICOToken.sol
+++ contracts/ICOToken.sol
@@ -10,8 +10,25 @@ contract MyICO is ERC20 {
     uint256 public constant SALE_DURATION = 1 days;
 
     // insert contructor() function
+    constructor(uint256 _amount) ERC20("myStery24", "ICO") {
+        owner = msg.sender;
+        _mint(owner, _amount);
+        startTime = block.timestamp;
+    }
 
     // insert ownerMint() function
+    function ownerMint(uint256 _amount) external {
+        require(msg.sender == owner, "Not the owner");
+        _mint(owner, _amount);
+    }
 
     // insert buyTokens() function
+    function buyTokens(uint256 _amount) external payable {
+        require(
+            block.timestamp <= startTime + SALE_DURATION,
+            "Sale has expired"
+        );
+        require(msg.value == (_amount * 1) / 10, "Wrong amount of ETH sent"); // 10% = 1/10
+        _mint(msg.sender, _amount);
+    }
 }
